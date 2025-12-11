d1fc0dc380cc6b38230e913331729134d78e3fec
Mohammad <m.ghavidel.v@gmail.com>
2025-12-11 01:20:29 +0100

    Initial commit of cpp_publisher_pkg and py_subscriber_pkg

---
+++ py_subscriber_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_subscriber_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mohammad',
+    maintainer_email='mohammad@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'minimal_subscriber = py_subscriber_pkg.subscriber_member_function:main',
+        ],
+    },
+)
